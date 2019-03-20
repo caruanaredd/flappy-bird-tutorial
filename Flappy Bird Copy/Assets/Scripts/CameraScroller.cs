2b04228fe28af9ec023c3c04ab6fc4a0e722bf4a
Redd <redd.caruana@mcast.edu.mt>
2019-03-20 13:29:18 +0100

    Environment Management
    Added CameraScroller script for horizontal movement.
    
    Added Ground script for seamless movement.
    
    Added tags for Ground, Pipes and Scoring.
    
    Added Pipes prefab.

---
+++ Flappy Bird Copy/Assets/Scripts/CameraScroller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScroller : MonoBehaviour
+{
+    [Tooltip("The speed by which the scene will move.")]
+    public float speed = 15f;
+
+    // Move the camera consistently to the right.
+    private void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+    }
+}
